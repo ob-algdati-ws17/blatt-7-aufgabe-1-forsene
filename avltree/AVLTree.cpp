7597a3910282f68e94d2a7f9b500e09d1509e5e6
meinkr4ft <32766044+meinkr4ft@users.noreply.github.com>
2018-01-03 06:45:31 +0100

    addest test directory, fixed CMakeLists files

--- avltree/AVLTree.cpp
+++ avltree/AVLTree.cpp
@@ -2,7 +2,3 @@
 // Created by Test on 03.01.2018.
 //
 
-int main() {
-    std::cout << "hi";
-    return 0;
-}
---
+++ test/testAVLTree.cpp
@@ -0,0 +1,8 @@
+#include "testAVLTree.h"
+#include <memory>
+
+using namespace std;
+
+TEST(test1, test2) {
+    EXPECT_EQ(nullptr, nullptr);
+}
